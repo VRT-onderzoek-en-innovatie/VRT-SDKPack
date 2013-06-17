b1db5536f89e609aa43c945c8a8615a1c916984f
Martijn Vandenberghe <martijnvandenberghe@gmail.com>
2013-06-17 15:17:52 +0200

    Initial Commit

---
+++ vrt-sdk/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  vrt-sdk
+//
+//  Created by Martijn Vandenberghe on 17/06/13.
+//  Copyright (c) 2013 Martijn Vandenberghe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ vrt-sdk/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  vrt-sdk
+//
+//  Created by Martijn Vandenberghe on 17/06/13.
+//  Copyright (c) 2013 Martijn Vandenberghe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
